f69870a25953d461ef63d66d434b54bd9dd30ff2
Moustapha Ndiaye <mikeend08@gmail.com>
2023-09-07 02:06:42 +0000

    tache

---
+++ main.js
@@ -0,0 +1,28 @@
+//Conditions IF
+/*let age = prompt('Saisir votre age');
+
+if (age < 18) {
+    alert('vous etes un mineur');
+} else if (age >= 70) {
+    alert('vous etes Senior');
+} else {
+    alert('vous etes Majeur');
+}*/
+
+//Condition SWITCH
+
+let age = prompt('Veuillez saisir votre age');
+
+if (age <= 14) {
+    alert('vous etes un enfant')
+} 
+else if (age <= 25) {
+    alert('vous etes Adolecent')
+} 
+else if (age <= 64) {
+    alert('vous etes Adulte')
+} 
+else {
+    alert('vous etes Aine')
+} 
+
